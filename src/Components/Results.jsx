0b35b5a8c7bce55fe5b1fb304d28b97f0e75be09
shreyansh <shreyansh.hurkat70@gmail.com>
2023-12-26 11:15:44 +0530

    First Working State for Search Functionality

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -14,12 +14,19 @@ function Results() {
   }, []);
 
   if (isLoading) return <Loading />
-  
+
   switch (location.pathname) {
     case '/websearch':
       return (
-        <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
-
+        <div className='flex flex-wrap justify-between space-y-6 sm:px-56 mt-5'>
+        {results?.results?.map(({ url, title }, index) => (
+            <div key={index} className="md:w-2/5 w-full">
+              <a href={url} target="_blank" rel="noreferrer">
+                <p className="text-sm">{url.length > 30 ? url.substring(0, 30) : url}</p>
+                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700  ">{title}</p>
+              </a>
+            </div>
+          ))}
         </div>
       )
     case '/imagesearch':
